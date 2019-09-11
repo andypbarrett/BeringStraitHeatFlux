2d4e7799aa3f7a76893f6f9685216b97ee0ca638
Andy Barrett <apbarret@nsidc.org>
2019-09-11 16:14:25 -0600

    select dirpath according to host

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,17 @@
-MOORING_DIRPATH='/home/apbarret/data/BeringStraitHeatFlux/Mooring'
-#MOORING_FILEFMT='BeringStrait_Monthlymeans_{:s}_Oct2017.txt'
+import socket
+
+mooring_dirpath = {
+    'nsidc-abarrett-442': '/home/apbarret/Data/BeringStraitHeatFlux/Mooring',
+    'nsidc-abarrett': '/home/apbarret/data/BeringStraitHeatFlux/Mooring',
+}
+MOORING_DIRPATH=mooring_dirpath[socket.gethostname()]
+
+reanalysis_dirpath = {
+    'nsidc-abarrett-442': '/home/apbarret/Data/BeringStraitHeatFlux/MERRA2',
+    'nsidc-abarrett': '/disks/arctic5_raid/abarrett/MERRA2/monthly', 
+}
+REANALYSIS_DIRPATH = reanalysis_dirpath[socket.gethostname()]
+
 MOORING_FILEPATH = {
     'HEAT': 'BeringStrait_Monthlymeans_HEAT_Oct2017.txt',
     'PH': 'BeringStrait_Monthlymeans_PH_April2019.txt',
@@ -8,5 +20,5 @@ MOORING_FILEPATH = {
     'WIND': 'BeringStrait_MonthlyMeans_WIND_May2019.txt',
     }
 
-REANALYSIS_DIRPATH = '/disks/arctic5_raid/abarrett/MERRA2/monthly'
+
 
